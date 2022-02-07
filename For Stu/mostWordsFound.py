2832b2d1ef51bb7bfa4b3295d93cd84e35bd9431
TuTlian <RenaTuT0401@gmail.com>
2022-02-07 17:46:11 +0800

    mostWordsFound

--- For Stu/mostWordsFound.py
+++ For Stu/mostWordsFound.py
@@ -4,6 +4,7 @@
 # 给你一个字符串数组sentences，其中sentences[i]表示单个 句子。
 #
 # 请你返回单个句子里 单词的最多数目。
+# 外面假设循环的是n次里面循环的是m次 所以是O(n * m ) 所以这个题目就是O(n * m) 如果m 等于n就是o(n^2)（该代码时间复杂度是O(n*m) n是字符串数组的长度 m是字符串的长度）
 sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
 
 i = 0
