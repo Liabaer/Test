929dcbdea20518c3c184697dd0ebb362bac71779
TuTlian <RenaTuT0401@gmail.com>
2022-05-08 19:42:45 +0800

    practice

--- python_level2_examination/practice5.py
+++ python_level2_examination/practice5.py
@@ -104,49 +104,49 @@
 
 # # 第二题
 # 使用字典和列表型变量完成某课程的考勤记录统计，某班有20名同学，姓名和年龄存储 在student--name.txt,某课程第一次考勤数据存储在class-data.csv中。
-# 问题1：求出缺勤考 试的名单存入un_score_.student.txt。
-import csv
+# # 问题1：求出缺勤考 试的名单存入un_score_.student.txt。
+# import csv
+# #
+# lun = []
+# fc = open('class-data(1).csv', 'r')
+# fc.seek(53)
+# fcn = csv.reader(fc)
+# # count = 0
+# ls_class = []
+# for i in fcn:
+#     # i是每一列csv的数组
+#     j = i[1]
+#     # count += 1
+#     lun.append(j)
+#     temp = [i[3], i[1], i[4], i[2]]
+#     ls_class.append(temp)
+#     # print(i)
+# fc.close()
+# # print(lun)
+# print(ls_class)
 #
-lun = []
-fc = open('class-data(1).csv', 'r')
-fc.seek(53)
-fcn = csv.reader(fc)
-# count = 0
-ls_class = []
-for i in fcn:
-    # i是每一列csv的数组
-    j = i[1]
-    # count += 1
-    lun.append(j)
-    temp = [i[3], i[1], i[4], i[2]]
-    ls_class.append(temp)
-    # print(i)
-fc.close()
-# print(lun)
-print(ls_class)
-
-fs = open('student-name(1).txt', 'r')
-fs.seek(9)
-fsn = fs.readlines()
-ln = []
-la = []
-num = 0
-cnt = 0
-dic_student = {}
-for i in fsn:
-    i = i.strip()
-    if i != '':
-        name = i.split(' ')[0]
-        age = i.split(' ')[1]
-        dic_student[name] = age
-        num += int(age)
-        cnt += 1
-        if name not in lun:
-            ln.append(name)
-        la.append(age)
-    # print(i)
-fs.close()
-print(dic_student)
+# fs = open('student-name(1).txt', 'r')
+# fs.seek(9)
+# fsn = fs.readlines()
+# ln = []
+# la = []
+# num = 0
+# cnt = 0
+# dic_student = {}
+# for i in fsn:
+#     i = i.strip()
+#     if i != '':
+#         name = i.split(' ')[0]
+#         age = i.split(' ')[1]
+#         dic_student[name] = age
+#         num += int(age)
+#         cnt += 1
+#         if name not in lun:
+#             ln.append(name)
+#         la.append(age)
+#     # print(i)
+# fs.close()
+# print(dic_student)
 # print(ln)
 
 # 问题2：统计学生的平均年龄、最大年龄和最小年龄。
@@ -154,19 +154,78 @@ print(dic_student)
 
 # 问题3：存储新的文件class-data-age.csv,文件记录iD、姓名、年龄、注册时间和班级，按照班级进行排序，班级相同，按照年龄升序排列
 
-for i in ls_class:
-    name = i[1]
-    age = dic_student[name]
-    i.insert(2, age)
-print(ls_class)
-ls_class.sort(key=lambda x: (x[4], x[2]) )
-print('---', ls_class)
-f_new = open('class-data-age.csv', 'w')
-f_new_w = csv.writer(f_new)
-f_new_w.writerow(['ID', '姓名', '年龄', '注册时间', '班级'])
-i = 0
-while i < len(ls_class):
-    f_new_w.writerow(ls_class[i])
-    i += 1
-f_new.close()
+# for i in ls_class:
+#     name = i[1]
+#     age = dic_student[name]
+#     i.insert(2, age)
+# print(ls_class)
+# ls_class.sort(key=lambda x: (x[4], x[2]) )
+# print('---', ls_class)
+# f_new = open('class-data-age.csv', 'w')
+# f_new_w = csv.writer(f_new)
+# f_new_w.writerow(['ID', '姓名', '年龄', '注册时间', '班级'])
+# i = 0
+# while i < len(ls_class):
+#     f_new_w.writerow(ls_class[i])
+#     i += 1
+# f_new.close()
 
+#
+# 第三题
+# 请编写程序，生成随机密码。具体要求如下：
+# (1)使用random库，采用0x1010作为随机数种子。
+# (2)密码abcdefghijkImnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*中的字符组 成。
+# (3)每个密码长度固定为15个字符
+# (4)程序运行每次产生10个密码，每个密码一行。
+# (5)每次产生的10个密码首字符不能一样。
+# (6) 密码的中间五位按照scⅱ码从小到大排序，最后五位从大到小排序，排序后如果生成的密码出现过，则继续随机 密码，然后排序直到该密码没有出现过。
+# (7）程序运行后产生的密码保存在“random_password.txt"文件中，每次允许代码不能覆盖原文件
+import random
+temp = 'abcdefghijkImnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*'
+random.seed(0x1010)
+dict_first_name = {}
+def create_pwd():
+    """
+    随机生成密码
+    :return:
+    """
+    ser = ''
+    i = 15
+    while i > 0:
+        ser += random.choice(temp)
+        i -= 1
+    return ser
+
+def be_pwd():
+    '''
+    判断首字母是否重复
+    :return:
+    '''
+    ser = ''
+    ls_ser = []
+    j = 10
+    while j > 0:
+        ser = create_pwd()
+        if ser[0] in dict_first_name:
+            continue
+        else:
+            dict_first_name[ser] = ser[0]
+        ls_ser.append(ser)
+        j -= 1
+    return ls_ser
+ls = be_pwd()
+print(ls)
+
+# print(be_pwd())
+
+f = open('random_password.txt', 'a')
+for i in ls:
+    m = list(i[5:10])
+    n = list(i[10:15])
+    m.sort()
+    n.sort(reverse=True)
+    # print(m, n)
+    res = i[0:5]+''.join(m)+''.join(n)
+    f.write('{}\n'.format(res))
+    print(res)
+f.close()
