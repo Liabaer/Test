fb7d0cd4aa630328efd59a6569e300fcef47cc60
TuTlian <RenaTuT0401@gmail.com>
2022-06-27 18:30:43 +0800

    mysql_pro

--- mysql_pro/test_api/courier_service.py
+++ mysql_pro/test_api/courier_service.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
+from mysql_pro.test_api.mysql_api import MysqlClient
+from  mysql_pro.test_api.mysql_Courier import MySQLCourier
+
 class CourierService(object):
     @staticmethod
-    def get_courier(courier):
+    def register_courier(courier):
+        courier = MySQLCourier()
+
+        MysqlClient.db.execute("insert into courier(id) values (%s,%s,%s,%s,%s,%s,%s)",
+                               (courier.id,courier.status,courier.delivery_type,courier.courier_location,
+                                courier.create_time,courier.last_online_time,courier.courier_email))
+        print('注册成功，注册的骑手id：'+str(courier.courier_id))
+
+
 
