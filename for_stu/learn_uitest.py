bbc95199c20b5be5b1c5fd3a559235e54a494ffe
TuTlian <RenaTuT0401@gmail.com>
2022-09-10 16:50:07 +0800

    study uitest

--- for_stu/learn_uitest.py
+++ for_stu/learn_uitest.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from selenium import webdriver
+
+
+
+driver = webdriver.Chrome(executable_path="/Users/Tu/Downloads/chromedriver")    # Chrome浏览器
+
+
+# 打开网页
+driver.get("https://www.baidu.com")
